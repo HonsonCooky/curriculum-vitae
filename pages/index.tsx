df0305727e7abe1300737e0beaef3b6e5c68f5b4
Harrison Cook <HonsonCooky@gmail.com>
2023-07-11 20:13:38 +1200

    implement catppucin theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
-import { Oswald } from 'next/font/google'
-import NavBar from './components/navbar'
+import NavBar from "@/components/navbar";
+import { Oswald } from "next/font/google";
 
-const font = Oswald({ subsets: ['latin'] })
+const font = Oswald({ subsets: ["latin"] });
 export default function Home() {
-
-
-
   return (
-    <main className={`flex min-h-full min-w-full ${font.className}`} >
-      <div className='absolute bg-dark-base w-full h-full'>
+    <main className={`flex min-h-full min-w-full ${font.className}`}>
+      <div className="absolute h-full w-full bg-light-base dark:bg-dark-base">
         <NavBar />
       </div>
     </main>
-  )
+  );
 }
