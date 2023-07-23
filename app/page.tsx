e51b47cf3fab512a75c0d8afd63615f96ff0c1b4
Harrison Cook <HonsonCooky@gmail.com>
2023-07-23 13:02:30 +1200

    Update home page welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,13 @@ export default function Home() {
         </h1>
         <h2 className="text-3xl mb-10">Software Engineer</h2>
         <p className="w-96 text-2xl font-thin text-justify">
-          <span className="text-light-green dark:text-dark-green">
+          <span className="text-light-blue dark:text-dark-blue">
             Haere Mai!{" "}
           </span>{" "}
           Hello! Welcome to the humble HTTP home of a happily hardworking{" "}
-          <span className="text-light-peach dark:text-dark-peach">
-            developer
-          </span>
-          . A hands-on{" "}
-          <span className="text-light-red dark:text-dark-red">happening</span>,
-          hosting the hearty heat hatched in his head. Hooked? Heed{" "}
-          <span className="text-light-yellow dark:text-light-yellow">high</span>{" "}
-          for{" "}
-          <span className="text-light-blue dark:text-dark-blue">
-            hyperlinks
-          </span>
-          .
+          developer.
+          <br /> A heuristic hands-on happening, hosting hearty heat hatched
+          from his head. Hooked? Heed high for hyperlinks .
         </p>
       </div>
     </div>
