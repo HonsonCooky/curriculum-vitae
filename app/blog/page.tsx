fc01309c0f3327241cb1c95a9493eb241337025e
Harrison Cook <HonsonCooky@gmail.com>
2023-07-27 22:41:35 +1200

    begin blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,6 +8,17 @@ export default function Blogs() {
       <h2 className="mb-[2vh] text-[3vh] leading-[3vh]">
         Ideas Worth Ignoring
       </h2>
+      <p className="px-[20vw] text-justify text-[2vh] font-light">
+        <span className="text-bold text-light-red dark:text-dark-red">
+          Warning:
+        </span>{" "}
+        You are about to enter the world of bais and opinions. Posts are more
+        akin to that of a journal entry; A casual documenation of my thoughts,
+        that enables me to clarify opinions and feelings swirling in my head. In
+        anticipation of alternative views, an anonymous comment section is
+        provided on each topic. I'm always open to new ideas, and would love to
+        hear about what I've missed.
+      </p>
     </div>
   );
 }
