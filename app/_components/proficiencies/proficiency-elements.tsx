98bfea12954523957d08d1cb515e8874af591091
Harrison Cook <HonsonCooky@gmail.com>
2023-08-03 00:17:44 +1200

    update proficiency repsonsive design

--- app/_components/proficiencies/proficiency-elements.tsx
+++ app/_components/proficiencies/proficiency-elements.tsx
@@ -54,13 +54,13 @@ export function ProficiencyElements(params: {
           className="group absolute -translate-x-[50%] -translate-y-[50%] rounded-full nm-flat-light-base-sm hover:z-10 dark:nm-flat-dark-base-sm"
         >
           <Image
-            className="block h-[max(10vh,4vw)] w-[max(10vh,4vw)] overflow-clip rounded-full object-contain p-[max(1vh,0.5vw)] group-hover:h-[max(11vh,6vw)] group-hover:w-[max(11vh,6vw)]"
+            className="block h-[max(6vh,5vw)] w-[max(6vh,5vw)] overflow-clip rounded-full object-contain group-hover:h-[max(7vh,6vw)] group-hover:w-[max(7vh,6vw)] lg:h-[max(10vh,4vw)] lg:w-[max(10vh,4vw)] lg:p-[max(1vh,0.5vw)] lg:group-hover:h-[max(11vh,6vw)] lg:group-hover:w-[max(11vh,6vw)]"
             src={`/${content.imgRef}-logo.png`}
             alt={content.imgRef}
             height={100}
             width={100}
           />
-          <div className="invisible absolute w-full -translate-y-[80%] rounded px-[0.1vh] text-center text-[max(2vh,1vw,10px)] nm-flat-light-base-sm group-hover:visible group-hover:z-10 dark:nm-flat-dark-base-sm">
+          <div className="invisible absolute w-full -translate-y-[80%] rounded px-[0.1vh] text-center nm-flat-light-base-sm group-hover:visible group-hover:z-10 dark:nm-flat-dark-base-sm lg:text-[max(2vh,1vw)]">
             {content.imgRef.charAt(0).toUpperCase() +
               content.imgRef
                 .slice(1)
