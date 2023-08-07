83aa1922b5979d0377eac654cbfa11f6bb9a76c9
Harrison Cook <HonsonCooky@gmail.com>
2023-08-07 18:04:09 +1200

    start comment on post

--- app/blogs/[tag]/[post]/_components/user-comment.tsx
+++ app/blogs/[tag]/[post]/_components/user-comment.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 export default function UserComment() {
   const [value, setValue] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const maxTextLength = 65_535;
 
   const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
     const val = evt.target?.value;
@@ -12,10 +13,15 @@ export default function UserComment() {
 
   return (
     <div
-      className="sticky bottom-[min(1vh,1vw)] flex max-h-[30vh] w-[50vw] flex-col self-center rounded-xl 
+      className="sticky bottom-[min(2vh,2vw)] flex max-h-[30vh] w-[50vw] flex-col self-center rounded-xl 
       px-[min(4vh,4vw)] py-[min(2vh,2vw)] nm-flat-light-base dark:nm-flat-dark-base"
     >
-      <label className="mb-[min(1vh,1vw)] text-2xl">Comment:</label>
+      <div className="flex w-full justify-between">
+        <label className="mb-[min(1vh,1vw)] text-2xl">Comment:</label>
+        <label className="mb-[min(1vh,1vw)] text-2xl">
+          {value.length}/{maxTextLength}
+        </label>
+      </div>
       <textarea
         id="review-text"
         onChange={handleChange}
@@ -23,8 +29,8 @@ export default function UserComment() {
         ref={textAreaRef}
         rows={1}
         value={value}
-        className="rounded-xl border-2 border-light-sapphire px-[min(2vh,2vw)] py-[min(1vh,1vw)] text-xl font-light 
-        nm-inset-light-base dark:border-dark-sapphire dark:nm-inset-dark-base"
+        className={`resize-none rounded-xl border-2 border-light-sapphire px-[min(2vh,2vw)] py-[min(1vh,1vw)] 
+        text-xl font-light nm-inset-light-base dark:border-dark-sapphire dark:nm-inset-dark-base`}
       />
     </div>
   );
