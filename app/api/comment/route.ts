2f10c69d147758ae094dc2fea2cc6c3ab3ecffe7
Harrison Cook <HonsonCooky@gmail.com>
2023-08-08 19:56:44 +1200

    add comment api endpoints

---
+++ app/api/comment/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    body.content = Buffer.from(JSON.stringify(body.content));
+    console.log(body);
+    await prisma.comment.create({ data: body });
+    return NextResponse.json("Comment sent", { status: 200 });
+  } catch (e: any) {
+    console.log(e);
+    let status = 500;
+    return NextResponse.json(e, { status });
+  }
+}
---
+++ app/api/comments/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { prisma } from "../../prisma";
+
+const searchParamUuidSchema = z.string().uuid().nonempty();
+
+export async function GET(req: NextRequest) {
+  try {
+    const id = searchParamUuidSchema.parse(
+      req.nextUrl.pathname.split("/").at(-1)
+    );
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId: id,
+      },
+    });
+    return NextResponse.json(comments, { status: 200 });
+  } catch (e: any) {
+    let status = 500;
+    if (e instanceof z.ZodError) {
+      e = e.issues;
+      status = 400;
+    } else {
+      e = e.toString();
+    }
+    return NextResponse.json(e, { status });
+  }
+}
