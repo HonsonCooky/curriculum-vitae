34c6ba2f2bbc904f846d2cd5cc155d01351e3c80
Harrison Cook <HonsonCooky@gmail.com>
2023-08-15 07:41:13 +1200

    update visual for project tile

--- app/projects/_components/project-tags.tsx
+++ app/projects/_components/project-tags.tsx
@@ -3,11 +3,14 @@ import { Project, ProjectTag } from "./projects";
 export default function ProjectTags(params: { project: Project }) {
   return (
     <div className="my-[min(1vh,1vw)] flex flex-row">
-      {params.project.tags.map(
-        (tag, i) =>
-          `${ProjectTag[tag].replaceAll("_", " ")}` +
-          (i < params.project.tags.length - 1 ? ", " : "")
-      )}
+      {params.project.tags.map((tag) => (
+        <div
+          className=" mr-[min(1vh,1vw)] rounded-full px-[min(1vh,1vw)] nm-flat-light-base dark:nm-flat-dark-base"
+          key={tag.toString()}
+        >
+          {ProjectTag[tag].replaceAll("_", " ")}
+        </div>
+      ))}
     </div>
   );
 }
