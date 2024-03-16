5ef6d93df8136fd767fda94acf0e7b5c1f392e33
Harrison Cook <HonsonCooky@gmail.com>
2024-03-16 14:48:10 +1300

    update front page

--- docs/pages/about-me/scripts.js
+++
@@ -1,6 +0,0 @@
-import { implementSearchBox } from "../../modules/searchbox.js";
-
-const searchBox = document.getElementById("page-search-input");
-const searchBtn = document.getElementById("page-search-btn");
-const searchArea = document.querySelector("main");
-implementSearchBox(searchBox, searchBtn, searchArea);
--- docs/pages/home/scripts.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("about-me").addEventListener("click", function() {
-  window.location.href = `${window.location.origin}/pages/about-me`;
-});
