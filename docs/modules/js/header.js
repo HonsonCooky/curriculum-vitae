13811e2482988b6daccdb6818d598f402ff5c32e
Harrison Cook <HonsonCooky@gmail.com>
2024-03-24 14:33:33 +1300

    start about page

--- docs/modules/js/header.js
+++ docs/modules/js/header.js
@@ -2,6 +2,9 @@ const header = document.querySelector("header");
 const navMenu = document.querySelector("nav");
 const navMobileBtn = document.getElementById("nav-mobile-btn");
 
+/**
+ * Show / Hide the mobile navigation menu
+ */
 function mobileNavMenuDisplay(show) {
   navMenu.style.cssText = `
   display: ${show ? "flex" : "none"};
@@ -13,6 +16,9 @@ function mobileNavMenuDisplay(show) {
   `;
 }
 
+/**
+ * React to mobile navigation menu icon click
+ */
 const folderIconStates = ["nf-fa-folder_o", "nf-fa-folder_open_o"];
 navMobileBtn.addEventListener("click", function() {
   let toAdd = navMobileBtn.classList.contains(folderIconStates[0]) ? 1 : 0;
@@ -22,13 +28,18 @@ navMobileBtn.addEventListener("click", function() {
   mobileNavMenuDisplay(toAdd === 1);
 });
 
+/**
+ * Remove Mobile Navigation Menu styling from navigation panel
+ */
 function resetNavMobileMenu() {
   navMenu.style.cssText = "";
   navMobileBtn.classList.add(folderIconStates[0]);
   navMobileBtn.classList.remove(folderIconStates[1]);
 }
 
-// Nav button interactions
+/**
+ * Setup navigation button interactions
+ */
 Array.from(header.querySelectorAll("button")).forEach((btn) => {
   let url = "#" + btn.innerText.replace(".\\", "") + "-section";
   if (btn.id === "logo") {
@@ -41,7 +52,10 @@ Array.from(header.querySelectorAll("button")).forEach((btn) => {
   });
 });
 
-// Toggle the nav between mobile and web depending on window size
+/**
+ * - Toggle the nav between mobile and web depending on window size
+ * - Set "Section" heights, based on the header height
+ */
 window.addEventListener("resize", function() {
   if (!window.matchMedia("(max-width: 600px)").matches) {
     resetNavMobileMenu();
@@ -53,12 +67,14 @@ window.addEventListener("scroll", function() {
   if (this.lastScrollY) {
     const direction = window.scrollY - this.lastScrollY;
     const isUp = direction < 0;
-    if (isUp && window.scrollY > header.scrollHeight) {
+    if (isUp && window.scrollY > 0) {
       header.style.background = "var(--base)";
       header.style.position = "sticky";
+      header.style.top = 0;
     } else {
       header.style.background = "transparent";
       header.style.position = "static";
+      header.style.top = "-10vh";
     }
   }
   this.lastScrollY = window.scrollY;
