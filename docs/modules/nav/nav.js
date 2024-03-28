5f01f81bb505e3d5296b8c582c59d93d04fca3e1
Harrison Cook <HonsonCooky@gmail.com>
2024-03-28 14:05:20 +1300

    nav bar update

--- docs/modules/nav/nav.js
+++ docs/modules/nav/nav.js
@@ -1,9 +1,17 @@
 const menuBtn = document.getElementById("menu-btn");
 
-menuBtn.addEventListener("click", function () {
+menuBtn.addEventListener("click", function() {
   if (menuBtn.classList.contains("open")) {
     menuBtn.classList.remove("open");
+    Array.from(menuBtn.classList)
+      .filter((c) => c.includes("nf-"))
+      .forEach((c) => menuBtn.classList.remove(c));
+    menuBtn.classList.add("nf-md-folder");
   } else {
     menuBtn.classList.add("open");
+    Array.from(menuBtn.classList)
+      .filter((c) => c.includes("nf-"))
+      .forEach((c) => menuBtn.classList.remove(c));
+    menuBtn.classList.add("nf-md-close");
   }
 });
