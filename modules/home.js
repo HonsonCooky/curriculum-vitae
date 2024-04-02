dbce1e3de797bbc58e9db1d1af2f7fbf7a59066f
Harrison Cook <HonsonCooky@gmail.com>
2024-04-02 23:51:01 +1300

    home page finish

--- modules/home.js
+++ modules/home.js
@@ -1,17 +1,20 @@
 const typingText = document.getElementById("typing-text");
 let currentPhrase = 0;
 const phrases = [
-  " Full Stack Developer",
-  " Frontend Developer",
-  " Backend Developer",
-  "n Integration Engineer",
-  "n Infrastructure Engineer",
+  "Full Stack Developer",
+  "Fontend",
+  "Backend",
+  "Mobile",
+  "Embedded",
+  "Cloud Computing",
 ];
 const typingSpeedBase = 100; // milliseconds
 
 function typeWriter(phrase, i) {
   if (i < phrase.length) {
-    typingText.innerHTML += phrase.charAt(i);
+    if (typingText.innerHTML.length > 0 && i === 0)
+      typingText.innerHTML = phrase.charAt(i);
+    else typingText.innerHTML += phrase.charAt(i);
     setTimeout(() => typeWriter(phrase, i + 1), typingSpeedBase);
   } else {
     setTimeout(deleteWriter, 1500);
@@ -19,13 +22,13 @@ function typeWriter(phrase, i) {
 }
 
 function deleteWriter() {
-  if (typingText.innerHTML.length > 0) {
-    typingText.innerText = typingText.innerText.slice(0, -1);
-    setTimeout(() => deleteWriter(), typingSpeedBase / 3);
-  } else {
+  if (typingText.innerHTML.length <= 1) {
+    typingText.innerHTML = "&nbsp;";
     setTimeout(nextPhrase, 1000);
     return;
   }
+  typingText.innerText = typingText.innerText.slice(0, -1);
+  setTimeout(() => deleteWriter(), typingSpeedBase / 3);
 }
 
 function nextPhrase() {
