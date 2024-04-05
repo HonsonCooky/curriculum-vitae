67eea65b2221290a6a9a43404142f6542ee8ed4f
Harrison Cook <HonsonCooky@gmail.com>
2024-04-05 21:35:32 +1300

    prettier override

--- modules/header/header.js
+++ modules/header/header.js
@@ -18,23 +18,21 @@ nav.addEventListener("click", toggleMenu);
 
 const navButtons = Array.from(nav.children);
 for (const btn of navButtons) {
-  btn.addEventListener("click", function() {
+  btn.addEventListener("click", function () {
     const sectionId = btn.id.replaceAll("-btn", "");
     const sectionElement = document.getElementById(sectionId);
     if (sectionElement) {
       sectionElement.scrollIntoView();
     } else {
-      console.log(`Missing ${sectionId}`)
+      console.log(`Missing ${sectionId}`);
     }
   });
 }
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", function () {
   if (nav.classList.contains(openTag)) toggleMenu();
-})
+});
 
-window.addEventListener("scroll", function() {
-  setTimeout(function() {
-
-  }, 0)
-})
+window.addEventListener("scroll", function () {
+  setTimeout(function () {}, 0);
+});
