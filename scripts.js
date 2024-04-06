d94c8cc26134f48778f38dbf556d5573e7c140c0
Harrison Cook <HonsonCooky@gmail.com>
2024-04-06 16:35:06 +1300

    start tui interface

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
-import "./modules/theme.js";
-import "./modules/header/header.js";
+/**--------------------------------------------------------------------------------------------------------------------
+  THEME 
+---------------------------------------------------------------------------------------------------------------------*/
+const themeAttr = "theme";
+const themeBtn = document.getElementById("theme-btn");
+
+function setTheme(isDark) {
+  const theme = isDark ? "dark" : "light";
+  const iconIndex = theme === "dark" ? 4 : 7;
+  document.documentElement.setAttribute(themeAttr, theme);
+  themeBtn.className = `nf nf-md-brightness_${iconIndex}`;
+}
+
+function toggleTheme() {
+  const curTheme = document.documentElement.getAttribute(themeAttr);
+  setTheme(!(curTheme === "dark"));
+}
+
+setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
+themeBtn.addEventListener("click", toggleTheme);
+
+/**--------------------------------------------------------------------------------------------------------------------
+  HEADER SIZE SCROLL MARGIN 
+---------------------------------------------------------------------------------------------------------------------*/
+const header = document.querySelector("header");
+document.documentElement.style.setProperty("--scroll-margin-top", header.scrollHeight + "px");
+window.addEventListener("resize", () => {
+  document.documentElement.style.setProperty("--scroll-margin-top", header.scrollHeight + "px");
+});
