345e4edf7759d5cbe68b94fb5eb7005ab5b1ab6a
Harrison Cook <HonsonCooky@gmail.com>
2024-04-07 03:09:15 +1200

    header and footer

--- scripts.js
+++ scripts.js
@@ -23,7 +23,39 @@ themeBtn.addEventListener("click", toggleTheme);
   HEADER SIZE SCROLL MARGIN 
 ---------------------------------------------------------------------------------------------------------------------*/
 const header = document.querySelector("header");
-document.documentElement.style.setProperty("--scroll-margin-top", header.scrollHeight + "px");
+document.documentElement.style.setProperty("--scroll-margin-top", header.offsetHeight + "px");
 window.addEventListener("resize", () => {
-  document.documentElement.style.setProperty("--scroll-margin-top", header.scrollHeight + "px");
+  document.documentElement.style.setProperty("--scroll-margin-top", header.offsetHeight + "px");
+});
+
+/**--------------------------------------------------------------------------------------------------------------------
+  NAVIGATION 
+---------------------------------------------------------------------------------------------------------------------*/
+const viewBtn = document.getElementById("view-btn");
+const viewBtnArrow = viewBtn.querySelector("i");
+const nav = header.querySelector("nav");
+const openTag = "open";
+
+function toggleNav() {
+  if (nav.classList.contains(openTag)) {
+    nav.classList.remove(openTag);
+    viewBtnArrow.style.rotate = "0deg";
+  } else {
+    nav.classList.add(openTag);
+    viewBtnArrow.style.rotate = "-180deg";
+  }
+}
+
+viewBtn.addEventListener("click", toggleNav);
+
+Array.from(nav.children).forEach((btn) => {
+  btn.addEventListener("click", function() {
+    window.location.href = `#${btn.innerText.toLowerCase()}-section`;
+  });
+});
+
+window.addEventListener("click", function(event) {
+  if (nav.classList.contains(openTag) && !(nav.contains(event.target) || viewBtn.contains(event.target))) {
+    toggleNav();
+  }
 });
