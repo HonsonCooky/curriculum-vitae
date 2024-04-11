4a7fafd0292a802e9249baec16ce35bf908089ee
Harrison Cook <HonsonCooky@gmail.com>
2024-04-12 09:57:19 +1200

    new header and footer

--- scripts.js
+++ scripts.js
@@ -22,46 +22,24 @@ themeBtn.addEventListener("click", toggleTheme);
 /**--------------------------------------------------------------------------------------------------------------------
   NAVIGATION 
 ---------------------------------------------------------------------------------------------------------------------*/
-const openTag = "open";
 const header = document.querySelector("header");
-const headerBtns = header.querySelectorAll(":scope > div > button");
-const headerPairs = Array.from(headerBtns).map((btn) => {
-  const nav = document.getElementById(btn.id.replaceAll("-btn", "-nav"));
-  return {
-    btn,
-    nav,
-  };
+document.documentElement.style.setProperty("--scroll-top", `${header.offsetHeight}px`);
+
+const pages = Array.from(document.querySelector("main").children);
+const threshold = window.innerHeight / 5;
+window.addEventListener("scroll", function() {
+  setTimeout(() => {
+    for (const page of pages) {
+      const rect = page.getBoundingClientRect();
+      const navBtnId = `${page.id}-btn`;
+      const navBtn = document.getElementById(navBtnId);
+      const icon = navBtn.querySelector("i");
+      if (rect.top < threshold && rect.bottom > threshold) {
+        console.log(navBtnId);
+        icon.className = "nf nf-fa-folder_open";
+      } else {
+        icon.className = "nf nf-fa-folder";
+      }
+    }
+  }, 0);
 });
-
-function toggleNav(btn, nav, off) {
-  if (nav.classList.contains(openTag) || off) {
-    btn.classList.remove(openTag);
-    nav.classList.remove(openTag);
-  } else {
-    btn.classList.add(openTag);
-    nav.classList.add(openTag);
-  }
-}
-
-for (const { btn, nav } of headerPairs) {
-  btn.addEventListener("click", () => {
-    headerPairs.forEach(({ btn: oBtn, nav: oNav }) => {
-      if (oBtn != btn) toggleNav(oBtn, oNav, true);
-    });
-    toggleNav(btn, nav);
-  });
-}
-
-window.addEventListener("click", function(event) {
-  for (const { btn, nav } of headerPairs) {
-    if (btn.contains(event.target) || nav.contains(event.target)) return;
-  }
-
-  for (const { btn, nav } of headerPairs) {
-    toggleNav(btn, nav, true);
-  }
-});
-
-/**--------------------------------------------------------------------------------------------------------------------
-  TEXT LOADER 
----------------------------------------------------------------------------------------------------------------------*/
