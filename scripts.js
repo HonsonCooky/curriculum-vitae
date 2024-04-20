8f90ff6c15e15eb4fa73c5fbba54556c6df0482f
Harrison Cook <HonsonCooky@gmail.com>
2024-04-20 14:42:49 +1200

    project description start

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+/**---------------------------------------------------------------------------------------------------------------------
+ THEME
+----------------------------------------------------------------------------------------------------------------------*/
+
 const themeAttr = "theme";
 const themeBtn = document.getElementById("theme-btn");
 
@@ -15,3 +19,13 @@ const isDefaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 setTheme(isDefaultDark);
 themeBtn.checked = isDefaultDark;
 themeBtn.addEventListener("click", toggleTheme);
+
+/**---------------------------------------------------------------------------------------------------------------------
+ HOME
+----------------------------------------------------------------------------------------------------------------------*/
+const industryExperience = document.getElementById("industry-years");
+const startDate = new Date("2021-08-01");
+const todayDate = new Date();
+const dateDiff = todayDate.getTime() - startDate.getTime();
+const years = dateDiff / (1000 * 60 * 60 * 24 * 365);
+industryExperience.innerHTML = years.toFixed(1);
