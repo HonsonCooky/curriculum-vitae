9ee0852b71a264dd85e3c03433ca4c66deff8a9f
Harrison Cook <HonsonCooky@gmail.com>
2024-04-25 00:17:28 +1200

    start tech eval keys

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 /**---------------------------------------------------------------------------------------------------------------------
  THEME
 ----------------------------------------------------------------------------------------------------------------------*/
-
 const themeAttr = "theme";
 const themeBtn = document.getElementById("theme-btn");
 
@@ -20,10 +19,14 @@ setTheme(isDefaultDark);
 themeBtn.checked = isDefaultDark;
 themeBtn.addEventListener("click", toggleTheme);
 
+function isMobile() {
+  return !window.matchMedia("(min-width: 769px)").matches;
+}
+
 /**---------------------------------------------------------------------------------------------------------------------
   CSS VARIABLES
 ----------------------------------------------------------------------------------------------------------------------*/
-const root = document.querySelector(":root");
+const root = document.documentElement;
 const footer = document.querySelector("footer");
 
 root.style.setProperty("--scroll-bottom", footer.offsetHeight + "px");
@@ -31,15 +34,52 @@ root.style.setProperty("--scroll-bottom", footer.offsetHeight + "px");
 /**---------------------------------------------------------------------------------------------------------------------
  HOME
 ----------------------------------------------------------------------------------------------------------------------*/
-const industryExperience = document.getElementById("industry-years");
-const workStart = new Date("2021-08-01");
-const today = new Date();
-const workingTime = today.getTime() - workStart.getTime();
-const workingYears = workingTime / (1000 * 60 * 60 * 24 * 365);
-industryExperience.innerHTML = workingYears.toFixed(1);
-
 const ageValue = document.getElementById("age-value");
+const today = new Date();
 const birthDate = new Date("1999-06-08");
 const ageTime = today.getTime() - birthDate.getTime();
 const ageYears = ageTime / (1000 * 60 * 60 * 24 * 365);
 ageValue.innerHTML = Math.floor(ageYears);
+
+const contactBtn = document.getElementById("contact-btn");
+contactBtn.addEventListener("click", () => {
+  footer.classList.add("expanded");
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+const resumeBtn = document.getElementById("resume-btn");
+
+/**---------------------------------------------------------------------------------------------------------------------
+ ABOUT
+----------------------------------------------------------------------------------------------------------------------*/
+const industryExperience = document.getElementById("industry-years");
+const workStart = new Date("2021-08-01");
+const workingTime = today.getTime() - workStart.getTime();
+const workingYears = workingTime / (1000 * 60 * 60 * 24 * 365);
+industryExperience.innerHTML = workingYears.toFixed(1);
+
+/**---------------------------------------------------------------------------------------------------------------------
+  FOOTER
+----------------------------------------------------------------------------------------------------------------------*/
+window.addEventListener("scroll", function() {
+  const isUp = (window.lastYPos ?? 0) > window.scrollY;
+  window.lastYPos = window.scrollY;
+
+  const atBottom = window.innerHeight + window.scrollY >= document.body.scrollHeight - 10;
+  if (atBottom && !footer.classList.contains("expanded") && !isUp) {
+    footer.classList.add("expanded");
+    if (!isMobile()) {
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+    return;
+  }
+
+  if (!footer.classList.contains("expanded")) return;
+
+  const footerRect = footer.getBoundingClientRect();
+  const footerYPos = window.innerHeight - footerRect.y;
+  const threshold = Number.parseInt(getComputedStyle(root).getPropertyValue("--scroll-bottom"));
+  if (footerYPos < threshold) {
+    footer.classList.remove("expanded");
+  }
+});
