4cbeb0c9ac88105f7484a0df359565c7037b8aeb
Harrison Cook <HonsonCooky@gmail.com>
2024-04-27 19:36:33 +1200

    start evaluations

--- scripts.js
+++ scripts.js
@@ -69,7 +69,6 @@ ageValue.innerHTML = Math.floor(ageYears);
 
 const contactBtn = document.getElementById("contact-btn");
 contactBtn.addEventListener("click", () => {
-  footer.classList.add("expanded");
   window.scrollTo(0, document.body.scrollHeight);
 });
 
@@ -94,11 +93,59 @@ function setupTechTables() {
     const tds = table.querySelectorAll("td");
     for (const td of tds) {
       td.addEventListener("click", function() {
-        const checkbox = td.querySelector("input");
+        const checkbox = this.querySelector("input");
         checkbox.checked = !checkbox.checked;
       });
     }
   }
 }
 
+function getChecked() {
+  const checked = [];
+  for (const table of tables) {
+    const checkedCells = [];
+    const tds = table.querySelectorAll("td");
+    for (const td of tds) {
+      const span = td.querySelector("span");
+      const checkbox = td.querySelector("input");
+      if (checkbox.checked) {
+        const itemId = span.innerText.toLowerCase().replace(".", " ").trim().replace(/[\s-]/g, "_");
+        checkedCells.push(itemId);
+      }
+    }
+
+    if (checkedCells.length > 0) {
+      checked[table.id] = checkedCells;
+    }
+  }
+  return checked;
+}
+
 setTimeout(setupTechTables, 0);
+
+/**---TECH CALCULATIONS-----------------------------------------------------------------------------------------------*/
+const parent = document.getElementById("calculations");
+const btn = parent.querySelector("button");
+
+const evaluations = {
+  languages: {
+    description: "",
+  },
+  environments: {
+    description: "",
+  },
+  tools: {
+    description: "",
+  },
+  devops: {
+    description: "",
+  },
+};
+
+btn.addEventListener("click", function() {
+  const checked = getChecked();
+  for (const key of Object.keys(checked)) {
+    const evals = evaluations[]
+    console.log(key, checked[key]);
+  }
+});
