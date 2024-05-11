836761a06ecaea07e9ff7c3089b5c069d408297c
Harrison <harrison.cook@z.co.nz>
2024-05-11 17:59:01 +1200

    device change

--- cv-builder/scripts.js
+++ cv-builder/scripts.js
@@ -6,22 +6,3 @@ const workingTime = today.getTime() - workStart.getTime();
 const workingYears = workingTime / (1000 * 60 * 60 * 24 * 365);
 const workingYearsRounded = Math.round(workingYears.toFixed(1) * 2) / 2;
 industryExperience.innerHTML = workingYearsRounded;
-
-// Skillset alignment
-const skillset = document.getElementById("skills");
-const skillsetLis = Array.from(skillset.querySelectorAll("li"));
-let maxPipe = skillsetLis.map((li) => Math.max(0, li.innerText.length - li.innerText.indexOf("|")));
-console.log(maxPipe);
-
-// for (const li of skillsetLis) {
-//   const text = li.innerText;
-//   const pipePos = ;
-//   if (pipePos === -1) break;
-//
-//   const postfixLength = text.length - pipePos;
-//   const diff = maxPipe - postfixLength;
-//   if (diff < 1) break;
-//
-//   const fill = " ".repeat(diff);
-//   li.innerHTML = `${text.slice(0, pipePos)}${fill}${text.slice(pipePos)}`;
-// }
