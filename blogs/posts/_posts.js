1dbf6f6be0a447a7d1a93d0d9a766c5067f088b7
Harrison Cook <HonsonCooky@gmail.com>
2024-07-23 12:16:27 +1200

    start blogs

--- blogs/posts/_posts.js
+++ blogs/posts/_posts.js
@@ -1,7 +1,7 @@
 const date = document.querySelector('meta[name="dcterms.created"]')?.content;
 if (date) {
-	const headerElement = document.querySelector("header");
-	const dateElement = document.createElement("h5");
-	dateElement.innerText = date;
-	headerElement?.appendChild(dateElement);
+  const headerElement = document.querySelector("header");
+  const dateElement = document.createElement("h5");
+  dateElement.innerText = date;
+  headerElement?.appendChild(dateElement);
 }
--- blogs/script.js
+++ blogs/script.js
@@ -1,39 +1,39 @@
 const blogList = document.getElementById("blog-list");
 const fetchFiles = await fetch(`${window.location.origin}/blogs/posts`).then((res) => res.text());
 const fileNames = Array.from(fetchFiles.matchAll(/title="([^"]*)"/g))
-  .map(([t]) => t.replaceAll('title="', "").replaceAll('"', ""))
-  .filter((t) => t.endsWith(".html") && !t.startsWith("_"));
+	.map(([t]) => t.replaceAll('title="', "").replaceAll('"', ""))
+	.filter((t) => t.endsWith(".html") && !t.startsWith("_"));
 
 const blogLinks = [];
 for (const fn of fileNames) {
-  const link = `${window.location.origin}/blogs/posts/${fn}`;
-  const file = await fetch(link)
-    .then((res) => res.text())
-    .then((txt) => new DOMParser().parseFromString(txt, "text/html"));
-  const head = file.querySelector("head");
-  const title = head.querySelector("title")?.innerText;
-  const description = head.querySelector('meta[name="description"]')?.content;
-  const date = head.querySelector('meta[name="dcterms.created"]')?.content;
+	const link = `${window.location.origin}/blogs/posts/${fn}`;
+	const file = await fetch(link)
+		.then((res) => res.text())
+		.then((txt) => new DOMParser().parseFromString(txt, "text/html"));
+	const head = file.querySelector("head");
+	const title = head.querySelector("title")?.innerText;
+	const description = head.querySelector('meta[name="description"]')?.content;
+	const date = head.querySelector('meta[name="dcterms.created"]')?.content;
 
-  const aTag = document.createElement("a");
-  aTag.href = link;
+	const aTag = document.createElement("a");
+	aTag.href = link;
 
-  const titleElement = document.createElement("h3");
-  titleElement.innerText = title;
-  aTag.appendChild(titleElement);
+	const titleElement = document.createElement("h3");
+	titleElement.innerText = title;
+	aTag.appendChild(titleElement);
 
-  const dateElement = document.createElement("h5");
-  dateElement.innerText = date;
-  aTag.appendChild(dateElement);
+	const dateElement = document.createElement("h5");
+	dateElement.innerText = date;
+	aTag.appendChild(dateElement);
 
-  const descriptionElement = document.createElement("p");
-  descriptionElement.innerText = description;
-  aTag.appendChild(descriptionElement);
+	const descriptionElement = document.createElement("p");
+	descriptionElement.innerText = description;
+	aTag.appendChild(descriptionElement);
 
-  blogLinks.push(aTag);
+	blogLinks.push(aTag);
 }
 
 blogList.innerHTML = "";
 for (const link of blogLinks) {
-  blogList.appendChild(link);
+	blogList.appendChild(link);
 }
